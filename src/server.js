0016587d919f9a10245472897a1a9fb119e6541b
Kiran Nambiar <kirannambiar12@gmail.com>
2019-12-12 23:50:13 +0530

    updates to deploy to heroku

--- src/server.js
+++ src/server.js
@@ -1,20 +1,18 @@
 //Install express server
 const express = require('express');
-const http = require('http');
 const path = require('path');
-const api = require('./server/routes/api');
+
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname + 'dist')));
+app.use(express.static(__dirname + '/dist/Tartl'));
 
-app.get('*', (req,res) => {
+app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Tartl/index.html'));
 });
 
-const port = process.env.PORT || '3001';
-app.set('port',port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('Running'))
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080,function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
