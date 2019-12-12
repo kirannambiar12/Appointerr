9f54d0ea90c5e18f39f3a1edfaade2906c0a0c2f
Kiran Nambiar <kirannambiar12@gmail.com>
2019-12-12 19:38:26 +0530

    start commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StartPageComponent } from './start-page/start-page.component';
+import { HomeComponent } from './home/home.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { EngineerComponent } from './engineer/engineer.component';
+import { ScientistComponent } from './scientist/scientist.component';
+import { MapComponent } from './map/map.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { ReminderComponent } from './reminder/reminder.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: '', component: StartPageComponent}, 
+  { path:'home',component: HomeComponent,
+children:[
+  {path: 'doc', component:DoctorComponent},
+  {path: 'eng', component:EngineerComponent},
+  {path: 'sci', component:ScientistComponent},
+ {path:'map', component: MapComponent},
+ {path:'app', component: AppointmentComponent},
+ {path:'rem', component: ReminderComponent},
+ {path:'rem', redirectTo: 'rem', pathMatch:'full' }
+ 
+
+
+
+]
+},
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { HomeComponent } from './home/home.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { EngineerComponent } from './engineer/engineer.component';
+import { ScientistComponent } from './scientist/scientist.component';
+import { MapComponent } from './map/map.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { ReminderComponent } from './reminder/reminder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
+    HomeComponent,
+    DoctorComponent,
+    EngineerComponent,
+    ScientistComponent,
+    MapComponent,
+    AppointmentComponent,
+    ReminderComponent
   ],
   imports: [
     BrowserModule,
